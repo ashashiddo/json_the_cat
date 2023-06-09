448b497c063305538ea2d154eaee8585d330a68c
ashashiddo <53973974+ashashiddo@users.noreply.github.com>
2023-06-09 09:31:04 -0400

    testing cats as a service assignment

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,18 +1,24 @@
 const request = require('request');
 
-const breedName = process.argv[2];
-
-const url = `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`;
-
-request(url, (error, response, body) => {
-  if (error) {
-    console.error('Error:', error);
-  } else {
-    const data = JSON.parse(body);
-    if (data.length === 0) {
-      console.log(`Breed '${breedName}' not found.`);
-    } else {
-      console.log(data[0].description);
-    }
-  }
-});
+const fetchBreedDescription = function(breedName) {
+  return new Promise((resolve, reject) => {
+    const url = `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`;
+  
+    request(url, (error, response, body) => {
+      if (error) {
+        reject(error);
+      } else {
+        const data = JSON.parse(body);
+        if (data.length === 0) {
+          reject(`Breed '${breedName}' not found.`);
+        } else {
+          resolve(data[0].description);
+        }
+      }
+    });
+  });
+};
+  
+  
+module.exports = { fetchBreedDescription };
+  
